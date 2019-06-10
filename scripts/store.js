c259052f5b260758e59731494495e179e003fef3
Blake Lowrey <blake.lowrey1@gmail.com>
2019-06-10 11:01:16 -0400

    added store.js

---
+++ scripts/store.js
@@ -0,0 +1,13 @@
+'use strict';
+const STORE = (function(){
+  return {
+    items: [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false }
+    ],
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+}());
