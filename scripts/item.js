364e8c0e6245c9363db16b2c51461297d4473517
Blake Lowrey <blake.lowrey1@gmail.com>
2019-06-10 14:59:55 -0400

    formatting

--- scripts/item.js
+++ scripts/item.js
@@ -3,26 +3,15 @@
 
 const Item = (function(){
   function validateName(name){
-<<<<<<< HEAD
     if(name === 'undefined' ){
       throw ('name does not exist');
-   }
     }
-    try {
-      validateName('undefined');
-    }
-    catch(e){
-      console.log(e);
-    }
-    function create(name){
-    return {
-        id: cuid(), name(name)
-  
-=======
-    if(typeof(name) === 'undefined' ){
-      throw TypeError('name does not exist');
->>>>>>> 611f595830c78d5e9afd2d4a91c7043ff9c77145
-    }
+  }
+  try {
+    validateName('undefined');
+  }
+  catch(e){
+    console.log(e);
   }
  
   function create(name){
